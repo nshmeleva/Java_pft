dc212f72e6d0450267e21e29e324c9ac36b97fbc
nshmeleva <32697798+nshmeleva@users.noreply.github.com>
2018-04-06 13:55:09 +0300

    Изменения 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,14 +8,12 @@ public class MyFirstProgram {
        hello("Nataliya");
 
        Square s = new Square(5);
-       System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
        Rectangle r = new Rectangle(4, 6);
-            System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area ());
+         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area ());
     }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody +  "!");
-    }
+    public static void hello(String somebody) {System.out.println("Hello, " + somebody +  "!"); }
 
 }
