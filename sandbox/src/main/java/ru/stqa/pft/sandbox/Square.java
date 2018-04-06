20f6ab083495e66ad37f154f783a4573770c72c3
nshmeleva <32697798+nshmeleva@users.noreply.github.com>
2018-04-06 13:08:44 +0300

    Изменения 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by nshmeleva on 06.04.2018.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
